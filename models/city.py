030c859ac38b7451395b98e43511534a489ca83f
Rediet <redietkfle@gmail.com>
2023-06-19 02:40:52 -0700

    DBStorage City

--- models/city.py
+++ models/city.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+"""
+Module maps class City
+to a Database Table.
+"""
+import models
+from models.base_model import BaseModel, Base
+from models.place import Place
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """
+    Creates Table 'cities' with
+    two Columns for storing name,
+    and reference to State table.
+    """
+    __tablename__ = 'cities'
+
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+    places = relationship("Place", cascade='all, delete', backref="cities")
