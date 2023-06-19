721332eecb758d0647df801f1aa6c1f38d9dc690
Rediet <redietkfle@gmail.com>
2023-06-19 02:38:37 -0700

    DBStorage Review

--- models/review.py
+++ models/review.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+"""
+Module maps class Review
+to a Database Table.
+"""
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """
+    Creates a Table 'reviews' with
+    three columns for storing text
+    and ids.
+    """
+    __tablename__ = "reviews"
+
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), nullable=False, ForeignKey("places.id")
+    user_id = Column(String(60), nullable=False, ForeignKey("users.id")
