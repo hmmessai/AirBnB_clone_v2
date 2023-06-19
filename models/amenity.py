b453ae1d3723cfba30ea7e8dce2891d0327537e5
Rediet <redietkfle@gmail.com>
2023-06-19 11:32:22 -0700

    DBStorage Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+Module maps class Amenity
+to a Database Table.
+"""
+import models
+from models.base_model import BaseModel, Base
+from models.place import Place, place_amenity
+from sqlalchemy import Column, String
+from sqlalchemy.ord import relationship
 
+class Amenity(BaseModel, Base):
+    """
+    Creates Table with one
+    column for storing name.
+    """
+    __tablename__ = "amenities"
 
-class Amenity(BaseModel):
-    name = ""
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity,
+            viewonly=False) 
