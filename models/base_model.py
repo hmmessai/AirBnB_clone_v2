7a19be66965553c73d2bd73d843dc73ca06cdec8
Rediet <redietkfle@gmail.com>
2023-06-20 07:17:15 -0700

    Pycodestyle update

--- models/base_model.py
+++ models/base_model.py
@@ -22,23 +22,26 @@ class BaseModel:
     updated_at = Column(DateTime, default=(datetime.utcnow()), nullable=False)
 
     def __init__(self, *args, **kwargs):
+        """Method initializes attributes.
+
+        Args:
+            args: non-keyworded argument list.
+            kwargs: keyworded argument list
         """
-        Instantiates a new model.
-        """
-        if not kwargs:
+
+        tformat = "%Y-%m-%dT%H:%M:%S.%f"
+
+        if kwargs != {} and kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value,
+                                                           tformat)
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
-
-            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
