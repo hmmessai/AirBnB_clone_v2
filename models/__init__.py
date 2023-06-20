2ef645d97f5a4273043b01de2834c84ef6b1e9da
Rediet <redietkfle@gmail.com>
2023-06-20 05:46:50 -0700

    Switch storage type

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""
+Module instantiates object
+of FileStorage or DBStorage.
+"""
+from os import getenv
 
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorag()
 
-storage = FileStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
