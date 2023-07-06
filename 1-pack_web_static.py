640f464ad530c2586ed132f8ed51297ccd94f092
Messai <messai@github.com>
2023-07-07 00:14:19 +0300

    addition to 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,9 +4,17 @@ Generates a .tgz archive from the contents of the web_static folder
 """
 from fabric.api import sudo, run, local
 from datetime import datetime
-
+import os
 
 def do_pack():
     datentime = str(datetime.datetime.now()).split(sep='.')[0]
     datentime = datentime.replace(' ', '').replace(':', '').replace('-', '')
-    local('tar -cvzf versions/web_static{}.tgz web_static'.format(datentime))
+    if not os.path.exists('versions'):
+        local('mkdir -p versions')
+
+    arc_path = 'versions/webstatic{}.tgz'.format(datentime)
+    stat = local('tar -cvzf arc_path web_static'.format(datentime))
+    if stat.succeeded:
+        return arc_path
+    else:
+        return None
