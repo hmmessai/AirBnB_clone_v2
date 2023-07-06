3c28f560bd46f9f8c09c7bd3f5b440918f7cda17
Messai <messai@github.com>
2023-07-06 09:06:17 +0300

    correction

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python3
 """
 Generates a .tgz archive from the contents of the web_static folder
 """
 from fabric.api import sudo, run, local
-from 
+from datetime import datetime
+
 
 def do_pack():
-	local(tar -cvzf "versions/web_static{}{}{}{}{}{}.tgz".format
-			() web_static)
+    datentime = str(datetime.datetime.now()).split(sep='.')[0]
+    datentime = datentime.replace(' ', '').replace(':', '').replace('-', '')
+    local('tar -cvzf versions/web_static{}.tgz web_static'.format(datentime))
