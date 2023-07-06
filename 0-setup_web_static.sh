8cad6f4e144f9935d35d843d0e3c4cb339ddf654
Messai <messai@github.com>
2023-07-06 09:14:07 +0300

    correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,21 +5,21 @@
 sudo apt-get install nginx
 
 # Make sure the folder /data/web_static/releases exist
-mkdir -p /data/web_staic/releases/test/
+sudo mkdir -p /data/web_staic/releases/test/
 
 # Make sure the folder /data/web_static/shared exists
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/shared/
 
 # Create a fake html file to test
-touch /data/web_static/releases/test/index.html
-echo 'Testing!' > /data/web_static/releases/test/index.html
+sudo touch /data/web_static/releases/test/index.html
+sudo echo 'Testing!' > /data/web_static/releases/test/index.html
 
 # Create symbolic link
-ln -s -d /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -d /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership to ubuntu user and group
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to server web_static
 replacement="server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}"
-sed -i "s/server_name _;/$replacement/" /etc/nginx/sites-available/default
+sudo sed -i "s/server_name _;/$replacement/" /etc/nginx/sites-available/default
