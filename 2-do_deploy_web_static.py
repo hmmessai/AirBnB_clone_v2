959394fb0ee5b198b8851bb521f43aa6048c5990
Messai <messai@github.com>
2023-07-07 18:52:21 +0300

    Added 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,9 +20,19 @@ def do_deploy(archive_path):
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-    put(archive_path, '/tmp/{}'.format(file))
-    run('tar -xvf /tmp/{} -C /data/web_static/releases/{}'.format(file, name))
-    run('rm /tmp/{}'.format(file))
-    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}'.format(name, name))
-    run('rm -rf /data/web_static/current')
-    run('ln -s /data/web_static/releases/{}/ /data/web_static/current')
+    if put(archive_path, '/tmp/{}'.format(file)).failed is True:
+        return False
+    if run('tar -xvf /tmp/{} -C /data/web_static/releases/{}'.
+            format(file, name)).failed is True:
+        return False
+    if run('rm /tmp/{}'.format(file)).failed:
+        return False
+    if run('mv /data/web_static/releases/{}/web_static/*'
+        '/data/web_static/releases/{}'.format(name, name)).failed is True:
+        return False
+    if run('rm -rf /data/web_static/current').failed is True:
+        return False
+    if run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.
+            format(name)).failed is True:
+        return False
+    return True
