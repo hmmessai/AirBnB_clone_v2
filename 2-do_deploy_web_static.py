2663ab6afca00aa385bc339d5573375ca4b499a3
Messai <messai@github.com>
2023-07-07 18:44:48 +0300

    Added 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """Script that distributes an archive to the web_servers"""
 import os
-from fabric import run, put
+from fabric.api import env, put, run
 
 env.user = 'ubuntu'
 env.hosts = ['web-01.hmmessai.tech', 'web-02.hmmessai.tech']
+
 
 def do_deploy(archive_path):
     """
     Deploying archive to a remote server
     Arguments:
-        archive_path(str): the path to the archive to 
+        archive_path(str): the path to the archive to
             be deployed on the server
     """
     if not os.path.isfile(archive_path):
@@ -22,8 +23,6 @@ def do_deploy(archive_path):
     put(archive_path, '/tmp/{}'.format(file))
     run('tar -xvf /tmp/{} -C /data/web_static/releases/{}'.format(file, name))
     run('rm /tmp/{}'.format(file))
-    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}'
-        .format(name, name))
+    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}'.format(name, name))
     run('rm -rf /data/web_static/current')
     run('ln -s /data/web_static/releases/{}/ /data/web_static/current')
-
