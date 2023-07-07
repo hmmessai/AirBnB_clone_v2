296c8010e3284fe2243dd701cc704d6fcc59183e
Messai <messai@github.com>
2023-07-07 19:03:40 +0300

    Added 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,19 +20,22 @@ def do_deploy(archive_path):
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-    if put(archive_path, '/tmp/{}'.format(file)).failed is True:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-    if run('tar -xvf /tmp/{} -C /data/web_static/releases/{}'.
+    if run("tar -xvf /tmp/{} -C /data/web_static/releases/{}".
             format(file, name)).failed is True:
         return False
-    if run('rm /tmp/{}'.format(file)).failed:
+    if run("rm /tmp/{}".format(file)).failed:
         return False
-    if run('mv /data/web_static/releases/{}/web_static/*'
-        '/data/web_static/releases/{}'.format(name, name)).failed is True:
+    if run("mv /data/web_static/releases/{}/web_static/*"
+        "/data/web_static/releases/{}".format(name, name)).failed is True:
         return False
-    if run('rm -rf /data/web_static/current').failed is True:
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
         return False
-    if run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
             format(name)).failed is True:
         return False
     return True
