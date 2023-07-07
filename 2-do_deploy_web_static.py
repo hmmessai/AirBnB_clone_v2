1b98e397547a21204fcd9f3da884d0455341c8ec
Messai <messai@github.com>
2023-07-07 22:54:40 +0300

    deployed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ import os
 from fabric.api import env, put, run
 
 env.user = 'ubuntu'
-env.hosts = ['web-01.hmmessai.tech', 'web-02.hmmessai.tech']
+env.hosts = ['web-02.hmmessai.tech', 'web-01.hmmessai.tech']
 
 
 def do_deploy(archive_path):
@@ -20,20 +20,22 @@ def do_deploy(archive_path):
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    if put(archive_path, "/tmp/{}".format(file)).failed:
         return False
 
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(file, name)).failed is True:
+    if run("mkdir -p /data/web_static/releases/{}".format(name)).failed:
+        return False
+
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(file, name)).failed:
         return False
     if run("rm /tmp/{}".format(file)).failed:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(name, name)).failed is True:
+    if run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(name, name)).failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
+    if run("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+    if run("rm -rf /data/web_static/current").failed:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)).failed is True:
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)).failed:
         return False
     return True
