6b0f0fcf7690ead76f9a1d181e287277a30c133e
Messai <messai@github.com>
2023-07-21 14:31:37 +0300

    Correct pycodestyle

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,10 +17,12 @@ def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return ("Hello HBNB!")
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'"""
     return ("HBNB")
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_route(text):
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -17,15 +17,18 @@ def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return ("Hello HBNB!")
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'"""
     return ("HBNB")
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c_route(text):
     text = text.replace("_", " ")
     return ("C {}".format(escape(text)))
+
 
 @app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
