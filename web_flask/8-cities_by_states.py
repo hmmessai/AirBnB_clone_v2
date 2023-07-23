247eb29b3085fd4bc3c6f83134a39db405b46c0c
Messai <messai@github.com>
2023-07-23 20:00:44 +0300

    8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,3 +2,28 @@
 """Starts a Flask web application
     Route:
         /cities_by_states - displays cities within states
+"""
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_state():
+    """Display a HTML page with all States
+    and the cities inside the State
+    """
+    states = storage.all(State)
+    return (render_template("8-cities_by_states", states=states))
+    
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
